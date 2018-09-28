bb7c0a342109fecab52be0b07c205ec4ec82da44
Nickghost inc <you@example.com>
2018-09-28 16:11:52 +0300

    models added

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+	picture = models.ImageField(upload_to='picture/')
+    name = models.CharField(max_length =60)
+    details = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
+    
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pub_date__date = date)
+        return news
+    
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
--- pictures/views.py
+++ pictures/views.py
@@ -1,38 +1,39 @@
-from django.http  import HttpResponse,Http404
-import datetime as dt
 from django.shortcuts import render,redirect
-# Create your views here.
+from django.http import HttpResponse, Http404
+# import datetime as dt
+# from .models import Article
+
+# # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+
+# def past_days_news(request,past_date):
     
-def past_days_news(request, past_date):
+#     try:
+#         # Converts data from the string Url
+#         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+#     except ValueError:
+#         # Raise 404 error when ValueError is thrown
+#         raise Http404()
+#         assert False
 
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+#     if date == dt.date.today():
+#         return redirect(news_today)
+    
+#     news = Article.days_news(date)
+#     return render(request, 'all-news/past-news.html', {"date":date,"news":news})
 
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
+# def search_results(request):
+    
+#     if 'article' in request.GET and request.GET["article"]:
+#         search_term = request.GET.get("article")
+#         searched_articles = Article.search_by_title(search_term)
+#         message = f"{search_term}"
 
-    if date == dt.date.today():
-        return redirect(news_of_day)
+#         return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
 
-    return render(request, 'all-news/past-news.html', {"date": date})
-
-
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day    
-
-def news_of_day(request):
-    date = dt.date.today()
-    return render(request, 'all-news/today-news.html', {"date": date,})    
+#     else:
+#         message = "You haven't searched for any term"
+#         return render(request, 'all-news/search.html',{"message":message})
