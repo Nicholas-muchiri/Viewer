61859bfb72b5a5ac6015719a991012064d8bbafb
Nickghost inc <you@example.com>
2018-09-30 20:00:05 +0300

    gallery settings modified

--- gallery/settings.py
+++ gallery/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pictures.apps.PicturesConfig',
-    'bootstrap3',
+    'bootstrap4',
+    
 ]
 
 MIDDLEWARE = [
