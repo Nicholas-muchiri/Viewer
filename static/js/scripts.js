96af54a1a7bede2415ddfca6a0990aab33e130eb
Nickghost inc <you@example.com>
2018-10-02 07:59:32 +0300

    copy link function added

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -34,3 +34,15 @@ $(document).ready(function(){
         toolTip.innerHTML = 'Copy Link'
     })
 })
+
+
+
+function copyLink(value) {
+    function handler(event){
+        event.clipboardData.setData('text/plain', value);
+        event.preventDefault();
+        document.removeEventListener('copy', handler, true);
+    }
+    document.addEventListener('copy', handler, true);
+    document.execCommand('copy');
+}
